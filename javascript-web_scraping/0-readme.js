1f68666e0411d8ee006f93a25a01d25750a01a12
root <znacer99@gmail.com>
2023-09-22 12:01:34 -0700

    0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const f = require('fs');
-const argv = process.argv[2];
 
-f.readFile(argv, 'utf-8', function (error, data) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(data);
+const fs = require('fs');
+const arg = process.argv[2];
+
+fs.readFile(arg, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+  console.log(data);
 });

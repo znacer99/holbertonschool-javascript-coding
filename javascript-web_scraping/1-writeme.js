0d1e0144b4590436225cfd87f2d60da0ad153a44
znacer99 <naceurez95@gmail.com>
2023-10-30 16:25:50 +0000

    1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const f = require('fs');
-const fileName = process.argv[2];
-const string = process.argv[3];
+const fs = require('fs');
+const filePath = process.argv[2];
+const fileContent = process.argv[3];
 
-f.writeFile(fileName, string, 'utf-8', function (error) {
-  if (error) {
-    console.error(error);
+fs.writeFile(filePath, fileContent, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
   }
 });

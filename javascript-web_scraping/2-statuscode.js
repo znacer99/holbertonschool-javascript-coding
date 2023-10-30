d267cfbb66d533eb8459734663c2b628124f6a55
znacer99 <naceurez95@gmail.com>
2023-10-30 16:29:36 +0000

    2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
-const request = require('request');
+// Make sure you have the "request" module installed. If you don't have it installed, you can install it using npm:
 
+// npm install request
+
+const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-  if (error) {
-    console.log(error.message);
-  }
-  console.log(`code: ${response.statusCode}`);
+    if (error) {
+        console.log(error.message);
+    }
+    console.log(`code: ${response.statusCode}`);
 })

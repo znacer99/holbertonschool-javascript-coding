c2133fe8237d09807b36886be264b27a985fe17f
znacer99 <naceurez95@gmail.com>
2023-10-30 17:01:28 +0000

    5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -9,9 +9,9 @@ const url = process.argv[2];
 const arg2 = process.argv[3];
 
 request(url, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-        fs.writeFile(arg2, body, (err) => {
-            if (err) throw err;
-        })
-    }
-})
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(arg2, body, (err) => {
+      if (err) throw err;
+    });
+  }
+});

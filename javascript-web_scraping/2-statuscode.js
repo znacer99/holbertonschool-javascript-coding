d45ccfb52ccf46138ec4bcff4c1f1ac4d35d672c
znacer99 <naceurez95@gmail.com>
2023-10-30 16:32:27 +0000

    2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -7,8 +7,8 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.log(error.message);
-    }
-    console.log(`code: ${response.statusCode}`);
+  if (error) {
+  console.log(error.message);
+  }
+  {console.log(`code: ${response.statusCode}`);
 })

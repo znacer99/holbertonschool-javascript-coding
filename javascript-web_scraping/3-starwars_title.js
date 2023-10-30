eef03806743ebd5c50c0ba5c31f9b9e6e6ccf8bd
znacer99 <naceurez95@gmail.com>
2023-10-30 16:44:29 +0000

    3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,14 +4,14 @@
 
 const request = require('request');
 const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/`;
+const url = 'https://swapi-api.hbtn.io/api/films/';
 
 const resultUrl = url + id;
 
 request(resultUrl, (error, response, body) => {
   if (error) {
     console.log(error);
-  }
+    }
   else {
     const data = JSON.parse(body);
     console.log(data.title);
